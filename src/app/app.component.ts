39044e0466c58801a820d6219547bcdb89c3bb03
Fichtner21 <ernest.fichtner@gmail.com>
2020-08-25 11:03:22 +0200

    Tested service worker module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,18 @@ import { Component, OnInit } from '@angular/core';
 //   title = 'pwa-angular';
 // }
 
-export class AppComponent implements OnInit {
-  
+export class AppComponent implements OnInit {  
 
-  constructor() { }
+  constructor(private swUpdate: SwUpdate) { }
 
   ngOnInit(): void {
-    
+    if(this.swUpdate.isEnabled){
+      this.swUpdate.available.subscribe(() => {
+        if(confirm("New version available. Load New Version?")){
+          window.location.reload();
+        }
+      })
+    }
   }
 
 }
