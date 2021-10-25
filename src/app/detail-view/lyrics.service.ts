efcb146ef12dbde29f4143ca6d1821880a41fa28
Fichtner21 <ernest.fichtner@gmail.com>
2021-10-25 14:51:00 +0200

    renew api

--- src/app/detail-view/lyrics.service.ts
+++ src/app/detail-view/lyrics.service.ts
@@ -8,9 +8,9 @@ import { Observable } from 'rxjs';
 })
 export class LyricsService {
 
-  public API_URL_3_CHART = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.artists.get?page=1&page_size=3&country=it&apikey=6bc8e4e006c6670df9b11cdeea097920";  
+  public API_URL_3_CHART = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.artists.get?page=1&page_size=3&country=pl&apikey=6bc8e4e006c6670df9b11cdeea097920";  
 
-  public API_URL_5_TOP_SONGS_IT = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=it&f_has_lyrics=1&apikey=6bc8e4e006c6670df9b11cdeea097920";
+  public API_URL_5_TOP_SONGS_IT = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=pl&f_has_lyrics=1&apikey=6bc8e4e006c6670df9b11cdeea097920";
 
   public API_URL_TOP_3_DE = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.artists.get?page=1&page_size=3&country=de&apikey=6bc8e4e006c6670df9b11cdeea097920";
 
